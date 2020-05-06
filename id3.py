a473b894808820d4be6344339c0715facff236a2
Paul Leitner <paul.leitner89@gmail.com>
2020-05-06 18:10:00 +0200

    added information gain by attribute, next split identification for loop

--- id3.py
+++ id3.py
@@ -1,18 +1,46 @@
 import numpy as np
 import pandas as pd
+from information_gain import entropy #, information_gain
+from math import log2
 
 
 class ID3Tree:
     def __init__(self,
                  data: pd.DataFrame,
+                 target_column: str = None,
                  nodes: dict = None):
-        # initializer, blank placeholder attributes
-        self.data = data
-        self.nodes = nodes
 
-    def entropy(self,
-                column: str):
-        assert column in self.data.columns, "column not found in "
+        self.data = data
+#        self.X = data.drop(columns=filter(lambda x: x != target_column, data.columns))
+        self.nodes = nodes
+        self.target_column = target_column
+#        self.y = data[target_column].values
+        self.outcomes = data[target_column].unique()
+
+#        assert target_column in self.data.columns, "dependent column not found in data"
+        assert len(self.outcomes) == 2, "classification target must have exactly two outcomes"
+
+    def information_gain(self,
+                         attribute):
+        outcomes = self.data[attribute].unique()
+        conditional_entropy = 0
+        for possibility in outcomes:
+            part = self.data[self.data[attribute] == possibility]
+            # get entropy of target after split
+            e = entropy(part[self.target_column] == self.outcomes[0])
+            # get size of column in relation to unsplit data
+            frac = part.shape[0] / self.data.shape[0]
+            conditional_entropy += e * frac
+
+        # compare conditional entropy of attribute to entropy of entire dataset
+        ig = entropy(self.data[self.target_column] == self.outcomes[0]) - conditional_entropy
+
+        return ig
+#        conditional_entropy =
+
+    def next_split(self):
+        information_gains = {key: self.information_gain(key) for key in self.data.columns if key != self.target_column}
+        return information_gains
 
     def fit(self,
             dependent_column: str):
@@ -38,6 +66,12 @@ class ID3Tree:
 if __name__ == '__main__':
     with open('./weather_decision.csv', mode='r') as file:
         test_data = pd.read_csv(file)
-    t = ID3Tree(data=test_data)
+    t = ID3Tree(data=test_data,
+                target_column='go_out')
 
     print(t)
+    for i in ['forecast', 'temperature', 'humidity', 'windy']:
+        print(t.information_gain(i))
+
+    print(t.next_split())
+#    print(t.entropy())
