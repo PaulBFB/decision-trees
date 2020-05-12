c4687648b077090dbdda63af998fe30ff4086b84
Paul Leitner <paul.leitner89@gmail.com>
2020-05-12 16:09:52 +0200

    OMGWTFBBQ recursion FINALLY works!

--- id3.py
+++ id3.py
@@ -9,49 +9,59 @@ class ID3Tree:
     def __init__(self,
                  data: pd.DataFrame,
                  target_column: str = None,
-                 nodes: dict = None):
+                 max_depth: int = None):
 
         self.data = data
-#        self.X = data.drop(columns=filter(lambda x: x != target_column, data.columns))
-        self.nodes = nodes
+        self.nodes = {0: {'no_split': data}}
+        self.rules_ = None
         self.target_column = target_column
-#        self.y = data[target_column].values
         self.outcomes = data[target_column].unique()
 
-#        assert target_column in self.data.columns, "dependent column not found in data"
+        assert target_column in self.data.columns, "dependent column not found in data"
         assert len(self.outcomes) == 2, "classification target must have exactly two outcomes"
 
     def information_gain(self,
-                         attribute):
-        outcomes = self.data[attribute].unique()
+                         attribute: str,
+                         data: pd.DataFrame = None
+                         ):
+
+        if data is None:
+            data = self.data
+
+        outcomes = data[attribute].unique()
         conditional_entropy = 0
-        for possibility in outcomes:
-            part = self.data[self.data[attribute] == possibility]
+
+        for value in outcomes:
+            part = data[data[attribute] == value]
             # get entropy of target after split
             e = entropy(part[self.target_column] == self.outcomes[0])
             # get size of column in relation to unsplit data
-            frac = part.shape[0] / self.data.shape[0]
+            frac = part.shape[0] / data.shape[0]
             conditional_entropy += e * frac
 
         # compare conditional entropy of attribute to entropy of entire dataset
         ig = entropy(self.data[self.target_column] == self.outcomes[0]) - conditional_entropy
 
         return ig
-#        conditional_entropy =
 
-    def next_split(self):
+    def next_split(self,
+                   data: pd.DataFrame = None):
         """
         check information gain for all columns that are left, get the largest, return with name and IG
         :return: dict()
         """
+
+        if data is None:
+            data = self.data
+
         split_attribute = {'attribute': None,
                            'information_gain': 0}
-        for attribute in filter(lambda x: x != self.target_column, self.data.columns):
-            potential_ig = self.information_gain(attribute)
+        for attribute in filter(lambda x: x != self.target_column, data.columns):
+            potential_ig = self.information_gain(attribute, data)
             if potential_ig > split_attribute['information_gain']:
                 split_attribute['attribute'] = attribute
-                split_attribute['information_gain'] = self.information_gain(attribute)
-#        information_gains = {key: self.information_gain(key) for key in self.data.columns if key != self.target_column}
+                split_attribute['information_gain'] = self.information_gain(attribute, data)
+
         return split_attribute
 
     def split(self):
@@ -66,22 +76,59 @@ class ID3Tree:
         parts = dict()
         # get all possible outcomes, split with .loc method (in order to return copies
         for outcome in self.data[split_on].unique():
-            parts[f'{split_on}|{outcome}'] = self.data.loc[self.data[split_on] == outcome]
+            parts[f'{split_on}|{outcome}'] = self.data.loc[self.data[split_on] == outcome].drop(columns=[split_on])
 
-        # todo: drop the column that the data was split on for recursion??
-        # todo: change output into dict with {[attribute|outcome]: [DataFrame]}
+        self.data.drop(columns=[split_on], inplace=True)
         # does that make sense - what if subsequent splits are identical? duplicate dict keys?
 
         return parts
 
-    def fit(self,
-            dependent_column: str):
-        pass
-        # recursive fit function - check all attributes, get information gain, split on largest OIG
-        # base cases: leaves are pure / no more attributes / no information gain (?)
+    def filter_data(self,
+                    attribute,
+                    value,
+                    data=None):
+        if data is None:
+            data = self.data
+        filtered = data.loc[self.data[attribute] == value].reset_index(drop=True)
+        return filtered
+
+    def find_rules(self,
+                   data=None,
+                   rules=None):
+
+        if data is None:
+            data = self.data
+
+        split_attribute = self.next_split(data)['attribute']
+#        print(split_attribute)
+
+        outcomes = data[split_attribute].unique()
+#        print(outcomes)
+
+        if rules is None:
+            rules = {}
+            rules[split_attribute] = {}
+
+        for outcome in outcomes:
+            filtered = self.filter_data(split_attribute, outcome, data)
+            values = filtered[self.target_column].unique()
+            #            print(values)
+            if len(values) == 1:
+#                print(rules)
+                rules[split_attribute][outcome] = values[0]
+#                print(rules)
+            else:
+                rules[split_attribute][outcome] = self.find_rules(filtered)
+#                                                                  rules)
+
+        return rules
+#        self.rules_ = rules
+
+    def fit(self):
+        rules = self.find_rules()
+        self.rules_ = rules
 
     def score(self,
-#              data: pd.DataFrame,
               dependent_column: str):
         pass
         # calculate accuracy of results
@@ -101,12 +148,13 @@ if __name__ == '__main__':
     t = ID3Tree(data=test_data,
                 target_column='go_out')
 
-    print(test_data.head())
-
-    print(t)
-    for i in ['forecast', 'temperature', 'humidity', 'windy']:
-        print(t.information_gain(i))
-
-    print(t.next_split())
-    split_test = t.split()
-    pprint(split_test)
+#    print(t.next_split())
+#    print(t.data.head())
+#    t.find_rules()
+    t.fit()
+    pprint(t.rules_)
+#    print(t.filter_data('forecast', 'sunny'))
+#    print(t.next_split(t.filter_data('forecast', 'rain')))
+#    d = t.filter_data('forecast', 'sunny')
+#    print(d)
+#    print(t.next_split(data=d))
